ea851292abf9f2556e5fd704541031d963df9f7a
rosen <rosen.diankov@mujin.co.jp>
2015-12-15 15:24:34 +0900

    removed mixins and organized more functions from binpickingtask to realtimetask

--- python/mujincontrollerclient/realtimerobotclient.py
+++ python/mujincontrollerclient/realtimerobotclient.py
@@ -3,13 +3,14 @@
 
 import copy
 
-from . import controllerclientbase, viewermixin, jogmixin
+from . import controllerclientbase
 
 import logging
 log = logging.getLogger(__name__)
 
-class RealtimeRobotControllerClient(controllerclientbase.ControllerClientBase, viewermixin.ViewerMixin, jogmixin.JogMixin):
-
+class RealtimeRobotControllerClient(controllerclientbase.ControllerClientBase):
+    """mujin controller client for realtimerobot task
+    """
     _robotname = None # name of the robot selected
     _robots = None # a dict of robot params
 
@@ -17,10 +18,10 @@ class RealtimeRobotControllerClient(controllerclientbase.ControllerClientBase, v
         super(RealtimeRobotControllerClient, self).__init__(**kwargs)
         self._robotname = robotname
         self._robots = robots
-
+        
     def GetRobotName(self):
         return self._robotname
-
+    
     def GetRobots(self):
         return self._robots
 
@@ -31,13 +32,13 @@ class RealtimeRobotControllerClient(controllerclientbase.ControllerClientBase, v
     def GetRobotDeviceIOUri(self):
         robots = self._robots or {}
         return robots.get(self._robotname, {}).get('robotDeviceIOUri', '')
-
+    
     def IsRobotControllerConfigured(self):
         return len(self.GetRobotControllerUri()) > 0
-
+    
     def IsRobotDeviceIOConfigured(self):
         return len(self.GetRobotDeviceIOUri()) > 0
-
+    
     def ExecuteCommand(self, taskparameters, robotname=None, toolname=None, useallrobots=False, robots=None, usewebapi=None, timeout=10, fireandforget=False):
         """wrapper to ExecuteCommand with robot info set up in taskparameters
 
@@ -68,6 +69,238 @@ class RealtimeRobotControllerClient(controllerclientbase.ControllerClientBase, v
         taskparameters['robotname'] = robotname
         log.debug('robotname = %s, robots = %r', robotname, robots)
         return super(RealtimeRobotControllerClient, self).ExecuteCommand(taskparameters, usewebapi=usewebapi, timeout=timeout, fireandforget=fireandforget)
+    
+    def ExecuteTrajectory(self, trajectoryxml, robotspeed=None, timeout=10, **kwargs):
+        """Executes a trajectory on the robot from a serialized Mujin Trajectory XML file.
+        """
+        taskparameters = {'command': 'ExecuteTrajectory',
+                          'trajectory': trajectoryxml,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, robotspeed=robotspeed, timeout=timeout)
+    
+    def GetJointValues(self, timeout=10, **kwargs):
+        """gets the current robot joint values
+        :return: current joint values in a json dictionary with
+        - currentjointvalues: [0,0,0,0,0,0]
+        """
+        taskparameters = {'command': 'GetJointValues'}
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+
+    def MoveToolLinear(self, goaltype, goals, toolname=None, timeout=10, robotspeed=None, **kwargs):
+        """moves the tool linear
+        :param goaltype: type of the goal, e.g. translationdirection5d
+        :param goals: flat list of goals, e.g. two 5d ik goals: [380,450,50,0,0,1, 380,450,50,0,0,-1]
+        :param toolname: name of the manipulator, default is self.toolname
+        
+        :param maxdeviationangle: how much the tool tip can rotationally deviate from the linear path
+        :param plannername:
+        
+        :param workspeed: [anglespeed, transspeed] in deg/s and mm/s
+        :param workaccel: [angleaccel, transaccel] in deg/s^2 and mm/s^2
+        :param worksteplength: discretization for planning MoveHandStraight, in seconds.
+        :param workminimumcompletetime: set to trajduration - 0.016s. EMU_MUJIN example requires at least this much
+        :param workminimumcompleteratio: in case the duration of the trajectory is now known, can specify in terms of [0,1]. 1 is complete everything
+        :param numspeedcandidates: if speed/accel are not specified, the number of candiates to consider
+        :param workignorefirstcollisionee: time, necessary in case initial is in collision, has to be multiples of step length?
+        :param workignorelastcollisionee: time, necessary in case goal is in collision, has to be multiples of step length?
+        :param workignorefirstcollision:
+
+        """
+        taskparameters = {'command': 'MoveToolLinear',
+                          'goaltype': goaltype,
+                          'goals': goals,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, robotspeed=robotspeed, toolname=toolname, timeout=timeout)
+
+    def MoveToHandPosition(self, goaltype, goals, toolname=None, envclearance=None, closegripper=0, robotspeed=None, timeout=10, **kwargs):
+        """Computes the inverse kinematics and moves the manipulator to any one of the goals specified.
+        :param goaltype: type of the goal, e.g. translationdirection5d
+        :param goals: flat list of goals, e.g. two 5d ik goals: [380,450,50,0,0,1, 380,450,50,0,0,-1]
+        :param toolname: name of the manipulator, default is self.toolname
+        :param envclearance: clearance in milimeter, default is self.envclearance
+        :param closegripper: whether to close gripper once the goal is reached, default is 0
+        """
+        if envclearance is None:
+            envclearance = self.envclearance
+        taskparameters = {'command': 'MoveToHandPosition',
+                          'goaltype': goaltype,
+                          'goals': goals,
+                          'envclearance': envclearance,
+                          'closegripper': closegripper,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, robotspeed=robotspeed, toolname=toolname, timeout=timeout)
+
+    def UpdateObjects(self, envstate, targetname=None, state=None, unit="m", timeout=10, **kwargs):
+        """updates objects in the scene with the envstate
+        :param envstate: a list of dictionaries for each instance object in world frame. quaternion is specified in w,x,y,z order. e.g. [{'name': 'target_0', 'translation_': [1,2,3], 'quat_': [1,0,0,0]}, {'name': 'target_1', 'translation_': [2,2,3], 'quat_': [1,0,0,0]}]
+        :param unit: unit of envstate
+        """
+        if targetname is None:
+            targetname = self.targetname
+        taskparameters = {'command': 'UpdateObjects',
+                          'objectname': targetname,
+                          'object_uri': u'mujin:/%s.mujin.dae' % (targetname),
+                          'envstate': envstate,
+                          'unit': unit,
+                          }
+        taskparameters.update(kwargs)
+        if state is not None:
+            taskparameters['state'] = json.dumps(state)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+    
+    def Grab(self, targetname, toolname=None, timeout=10, **kwargs):
+        """grabs an object with tool
+        :param targetname: name of the object
+        :param toolname: name of the manipulator, default is self.toolname
+        """
+        taskparameters = {'command': 'Grab',
+                          'targetname': targetname,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, toolname=toolname, timeout=timeout)
+
+    def Release(self, targetname, timeout=10, **kwargs):
+        """releases an object already grabbed
+        :param targetname: name of the object
+        """
+        taskparameters = {'command': 'Release',
+                          'targetname': targetname,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+    
+    def GetGrabbed(self, timeout=10, **kwargs):
+        """gets the names of the grabbed objects
+        :return: names of the grabbed object in a json dictionary, e.g. {'names': ['target_0']}
+        """
+        taskparameters = {'command': 'GetGrabbed',
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+    
+    def GetTransform(self, targetname, unit='mm', timeout=10, **kwargs):
+        """gets the transform of an object
+        :param targetname: name of the object
+        :param unit: unit of the result translation
+        :return: transform of the object in a json dictionary, e.g. {'translation': [100,200,300], 'rotationmat': [[1,0,0],[0,1,0],[0,0,1]], 'quaternion': [1,0,0,0]}
+        """
+        taskparameters = {'command': 'GetTransform',
+                          'targetname': targetname,
+                          'unit': unit,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+    
+    def SetTransform(self, targetname, translation, unit='mm', rotationmat=None, quaternion=None, timeout=10, **kwargs):
+        """sets the transform of an object
+        :param targetname: name of the object
+        :param translation: list of x,y,z value of the object in milimeter
+        :param unit: unit of translation
+        :param rotationmat: list specifying the rotation matrix in row major format, e.g. [1,0,0,0,1,0,0,0,1]
+        :param quaternion: list specifying the quaternion in w,x,y,z format, e.g. [1,0,0,0]
+        """
+        taskparameters = {'command': 'SetTransform',
+                          'targetname': targetname,
+                          'unit': unit,
+                          'translation': translation,
+                          }
+        taskparameters.update(kwargs)
+        if rotationmat is not None:
+            taskparameters['rotationmat'] = rotationmat
+        if quaternion is not None:
+            taskparameters['quaternion'] = quaternion
+        if rotationmat is None and quaternion is None:
+            taskparameters['quaternion'] = [1, 0, 0, 0]
+            log.warn('no rotation is specified, using identity quaternion ', taskparameters['quaternion'])
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+    
+    def GetOBB(self, targetname, unit='mm', timeout=10, **kwargs):
+        """ Get the oriented bounding box of object
+        :param targetname: name of the object
+        :param unit: unit of the OBB
+        :return: OBB of the object
+        """
+        taskparameters = {'command': 'GetOBB',
+                          'targetname': targetname,
+                          'unit': unit,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+
+    def GetInnerEmptyRegionOBB(self, targetname, linkname=None, unit='mm', timeout=10, **kwargs):
+        """ Get the inner empty oriented bounding box of a container
+        :param targetname: name of the object
+        :param linkname: can target a specific link
+        :param unit: unit of the OBB
+        :return: OBB of the object
+        """
+        taskparameters = {'command': 'GetInnerEmptyRegionOBB',
+                          'targetname': targetname,
+                          'unit': unit,
+                          }
+        if linkname is not None:
+            taskparameters['linkname'] = unicode(linkname)
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+
+    def GetAABB(self, targetname, unit='mm', timeout=10, **kwargs):
+        """Gets the axis aligned bounding box of object
+        :param targetname: name of the object
+        :param unit: unit of the AABB
+        :return: AABB of the object, e.g. {'pos': [1000,400,100], 'extents': [100,200,50]}
+        """
+        taskparameters = {'command': 'GetAABB',
+                          'targetname': targetname,
+                          'unit': unit,
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
+    
+    def RemoveObjectsWithPrefix(self, prefix=None, prefixes=None, timeout=10, usewebapi=None, **kwargs):
+        """removes objects with prefix
+        """
+        taskparameters = {'command': 'RemoveObjectsWithPrefix',
+                          }
+        taskparameters.update(kwargs)
+        if prefix is not None:
+            taskparameters['prefix'] = unicode(prefix)
+        if prefixes is not None:
+            taskparameters['prefixes'] = [unicode(prefix) for prefix in prefixes]
+        return self.ExecuteCommand(taskparameters, timeout=timeout, usewebapi=usewebapi)
+    
+    def GetTrajectoryLog(self, timeout=10, **kwargs):
+        """Gets the recent trajectories executed on the binpicking server. The internal server keeps trajectories around for 10 minutes before clearing them.
+
+        :param startindex: int, start of the trajectory to get. If negative, will start counting from the end. For example, -1 is the last element, -2 is the second to last element.
+        :param num: int, number of trajectories from startindex to return. If 0 will return all the trajectories starting from startindex
+        :param includejointvalues: bool, If True will include timedjointvalues, if False will just give back the trajectories. Defautl is False
+
+        :return:
+
+        total: 10
+        trajectories: [
+        {
+        "timestarted": 12345215
+        "name": "movingtodest",
+        "numpoints": 100,
+        "duration": 0.8,
+        "timedjointvalues": [0, 0, 0, .....]
+        },
+        { ... }
+        ]
+
+        Where timedjointvalues is a list joint values and the trajectory time. For a 3DOF robot sampled at 0.008s, this is
+        [J1, J2, J3, 0, J1, J2, J3, 0.008, J1, J2, J3, 0.016, ...]
+
+        """
+        taskparameters = {'command': 'GetTrajectoryLog',
+                          }
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, timeout=timeout)
     
     def ChuckGripper(self, robotname=None, timeout=10, usewebapi=None, **kwargs):
         """chucks the manipulator
@@ -193,3 +426,22 @@ class RealtimeRobotControllerClient(controllerclientbase.ControllerClientBase, v
         }
         taskparameters.update(kwargs)
         return self.ExecuteCommand(taskparameters, timeout=timeout, usewebapi=usewebapi)
+    
+    #
+    # jogging related
+    #
+    def SetJogModeVelocities(self, jogtype, movejointsigns, robotname=None, toolname=None, robotspeed=None, robotaccelmult=None, usewebapi=False, timeout=1, fireandforget=False, **kwargs):
+        taskparameters = {
+            'command': 'SetJogModeVelocities',
+            'jogtype': jogtype,
+            'movejointsigns': movejointsigns,
+        }
+        if toolname is not None:
+            taskparameters['toolname'] = toolname
+        if robotspeed is not None:
+            taskparameters['robotspeed'] = robotspeed
+        if robotaccelmult is not None:
+            taskparameters['robotaccelmult'] = robotaccelmult
+        taskparameters.update(kwargs)
+        return self.ExecuteCommand(taskparameters, robotname=robotname, usewebapi=usewebapi, timeout=timeout, fireandforget=fireandforget)
+    
