201e666894f40002a47499a26763424b9dea3d17
rosen <rosen.diankov@mujin.co.jp>
2016-08-11 08:04:10 +0900

    added fireandforget parameters to necessary functions

--- python/mujincontrollerclient/realtimerobotclient.py
+++ python/mujincontrollerclient/realtimerobotclient.py
@@ -267,7 +267,7 @@ class RealtimeRobotControllerClient(planningclient.PlanningControllerClient):
         taskparameters.update(kwargs)
         return self.ExecuteCommand(taskparameters, timeout=timeout)
     
-    def RemoveObjectsWithPrefix(self, prefix=None, prefixes=None, timeout=10, usewebapi=None, **kwargs):
+    def RemoveObjectsWithPrefix(self, prefix=None, prefixes=None, timeout=10, usewebapi=None, fireandforget=False, **kwargs):
         """removes objects with prefix
         """
         taskparameters = {'command': 'RemoveObjectsWithPrefix',
@@ -277,7 +277,7 @@ class RealtimeRobotControllerClient(planningclient.PlanningControllerClient):
             taskparameters['prefix'] = unicode(prefix)
         if prefixes is not None:
             taskparameters['prefixes'] = [unicode(prefix) for prefix in prefixes]
-        return self.ExecuteCommand(taskparameters, timeout=timeout, usewebapi=usewebapi)
+        return self.ExecuteCommand(taskparameters, timeout=timeout, usewebapi=usewebapi, fireandforget=fireandforget)
     
     def GetTrajectoryLog(self, timeout=10, **kwargs):
         """Gets the recent trajectories executed on the binpicking server. The internal server keeps trajectories around for 10 minutes before clearing them.
