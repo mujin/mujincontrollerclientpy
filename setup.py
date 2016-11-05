e1ccf0548f0e761fd09b9a0a40b978d92ee7f320
rosen <rosen.diankov@mujin.co.jp>
2016-11-05 14:18:43 +0900

    add scene registertation helper

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version='0.0.1',
     packages=['mujincontrollerclient',],
     package_dir={'mujincontrollerclient':'python/mujincontrollerclient'},
+    scripts=['bin/mujin_controllerclientpy_registerscene.py'],
     locale_dir='locale',
     license='Apache License, Version 2.0',
     long_description=open('README.rst').read(),
