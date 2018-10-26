3ead0e88af64e989ddffa1bc6987dad04c99ba3c
rosen <rosen.diankov@mujin.co.jp>
2018-10-26 17:39:04 +0900

    fix typo raising error

--- python/mujincontrollerclient/uriutils.py
+++ python/mujincontrollerclient/uriutils.py
@@ -41,14 +41,14 @@ def _ParseURI(uri, allowfragments=True, fragmentseparator='@'):
     /测试_test.mujin.dae@body0_motion
     """
     if not isinstance(uri, unicode):
-        log.warn("_ParseURI uri should be unicode, current type is %s. Trying to decode utf-8" % type(uri))
+        log.warn("_ParseURI uri %r should be unicode, current type is %s. Trying to decode utf-8" % (uri, type(uri)))
         uri = uri.decode('utf-8')   # try using decode utf-8 if the input is not unicode
     i = uri.find(':')
     if not uri[:i].lower() == "mujin":
         # if scheme is not mujin specified, use the standard uri parse
         # TODO: figure out who calls this with non-mujin scheme
         # TODO: simon claim that there is conversion between mujin scheme and file scheme, all of them are done inside openrave, please verify, maybe remove this case
-        log.warn("_ParseURI found non-mujin scheme with uri %s" % uri) # for test usage, found out if there are non-mujin scheme be called.
+        log.warn("_ParseURI found non-mujin scheme with uri %r" % uri) # for test usage, found out if there are non-mujin scheme be called.
         r = urlparse(uri, allowfragments)
         return ParseResult(r.scheme, r.netloc, r.path.decode('utf-8'), r.params, r.query, r.fragment)  # make all uri path, no matter what scheme it is, to be utf-8 unicode.
     else:
@@ -630,7 +630,7 @@ class MujinResourceIdentifier(object):
         elif filename:
             self._InitFromFilename(filename)
         else:
-            raise MujinResourceIdentifier("Lack of parameters. initialization must include one of uri, primarykey, parttype or filename")
+            raise MujinResourceIdentifierError("Lack of parameters. initialization must include one of uri, primarykey, parttype or filename")
 
     @property
     def uri(self):
