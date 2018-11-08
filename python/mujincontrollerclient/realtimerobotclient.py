eac6319c1c9af4aae89f6f2a1022ad297bea8508
Ziyan Zhou <ziyan.zhou@mujin.co.jp>
2018-11-08 10:26:02 +0900

    Remove robotname from ResetSafetyFault
    It is not meaningful.

--- python/mujincontrollerclient/realtimerobotclient.py
+++ python/mujincontrollerclient/realtimerobotclient.py
@@ -633,11 +633,11 @@ class RealtimeRobotControllerClient(planningclient.PlanningControllerClient):
         }
         return self.ExecuteCommand(taskparameters, robotname=robotname, timeout=timeout, fireandforget=fireandforget)
 
-    def ResetSafetyFault(self, robotname=None, timeout=3, fireandforget=False):
+    def ResetSafetyFault(self, timeout=3, fireandforget=False):
         taskparameters = {
             'command': 'ResetSafetyFault',
         }
-        return self.ExecuteCommand(taskparameters, robotname=robotname, timeout=timeout, fireandforget=fireandforget)
+        return self.ExecuteCommand(taskparameters, timeout=timeout, fireandforget=fireandforget)
 
     def SetRobotBridgeControlMode(self, controlMode, timeout=3, fireandforget=False):
         taskparameters = {
