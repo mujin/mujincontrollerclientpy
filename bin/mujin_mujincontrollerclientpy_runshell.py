83ce89a4b4c63a466f37d5317534a12298dcaa17
Ziyan <ziyan.zhou@mujin.co.jp>
2019-03-26 16:47:23 +0900

    Rename field of orderCycleLog.

---
+++ bin/mujin_mujincontrollerclientpy_runshell.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from mujincontrollerclient.controllerclientbase import ControllerClient
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def _ParseArguments():
+    import argparse
+    parser = argparse.ArgumentParser(description='Open a shell to use controllerclient')
+    parser.add_argument('--loglevel', type=str, default=None, help='the python log level, e.g. DEBUG, VERBOSE, ERROR, INFO, WARNING, CRITICAL (default: %(default)s)')
+    parser.add_argument('--url', type=str, default='http://localhost', help='url of the controller (default: %(default)s)')
+    parser.add_argument('--username', type=str, default='mujin', help='username to login with (default: %(default)s)')
+    parser.add_argument('--password', type=str, default='mujin', help='password to login with (default: %(default)s)')
+    return parser.parse_args()
+
+
+def _ConfigureLogging(level=None):
+    try:
+        import mujincommon
+        mujincommon.ConfigureRootLogger(level=level)
+    except ImportError:
+        logging.basicConfig(format='%(levelname)s %(name)s: %(funcName)s, %(message)s', level=logging.DEBUG)
+
+
+def _Main():
+    options = _ParseArguments()
+    _ConfigureLogging(options.loglevel)
+
+    self = ControllerClient(options.url, options.username, options.password)
+
+    from IPython.terminal import embed
+    ipshell = embed.InteractiveShellEmbed(config=embed.load_default_config())(local_ns=locals())
+
+
+if __name__ == "__main__":
+    _Main()
