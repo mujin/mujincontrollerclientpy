a80ad0bfc4540c05ee2fc0e1558e6a1172b00732
simon.zheng <simon.zheng@mujin.co.jp>
2019-07-04 16:30:42 +0900

    add script for setting up planningserver log level

---
+++ bin/mujin_controllerclientpy_setplanningserverloglevel.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import os
+import argparse
+import json
+import mujincommon
+from mujincontrollerclient import uriutils, planningclient
+
+
+def ConfigurePlanningserver(level, config, taskzmqport=11000):
+    controllerurl = config.get('controllerurl', '').strip() or 'http://127.0.0.1'
+    routerport = os.environ.get('MUJIN_PLANNINGSERVER_MASTER_ROUTER_PORT', taskzmqport)
+    client = planningclient.PlanningControllerClient(
+        taskzmqport=routerport,
+        taskheartbeatport=None, 
+        taskheartbeattimeout=None, 
+        tasktype=None, 
+        scenepk=uriutils.GetPrimaryKeyFromURI(config.get('sceneuri', '')),
+        usewebapi=False, 
+        controllerurl=controllerurl)
+    client.SetLogLevel(level)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Set planningserver logging level')
+    parser.add_argument('--loglevel', action='store', type=str, dest='logLevel', default=None, help='Set logging level for webstack')
+    parser.add_argument('--conf', action='store', type=str, dest='conf', default=None, help='binpicking conf')
+    options = parser.parse_args()
+    configFilename = options.conf
+    if configFilename is None:
+        configFilename = mujincommon.GetExistingConfigFilename()
+    with open(configFilename, 'r') as f:
+        config = json.loads(f.read())
+    ConfigurePlanningserver(options.logLevel, config)
