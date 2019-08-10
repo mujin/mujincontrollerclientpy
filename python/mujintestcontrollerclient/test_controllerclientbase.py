b4a6b58b5563d55c7cf93214aa58491067182238
Ziyan Zhou <ziyan.zhou@mujin.co.jp>
2019-08-10 09:38:07 +0900

    Wrap the RESTful API response objects list to provide meta data for pagination.

--- python/mujintestcontrollerclient/test_controllerclientbase.py
+++ python/mujintestcontrollerclient/test_controllerclientbase.py
@@ -29,5 +29,16 @@ def test_RestartController():
 
 def test_GetScenes():
     with requests_mock.Mocker() as mock:
-        mock.get('http://controller/api/v1/scene/?format=json&limit=0&offset=0', json={'objects': []})
-        ControllerClient('http://controller', 'mujin', 'mujin').GetScenes() == []
+        mock.get('http://controller/api/v1/scene/?format=json&limit=0&offset=0', json={
+            'objects': [],
+            'meta': {
+                'total_count': 101,
+                'limit': 20,
+                'offset': 0,
+            },
+        })
+        scenes = ControllerClient('http://controller', 'mujin', 'mujin').GetScenes()
+        assert len(scenes) == 0
+        assert scenes.offset == 0
+        assert scenes.limit == 20
+        assert scenes.totalCount == 101
