2053f2beb43a6c0b5049348ee675f9181dbced15
Ziyan <ziyan.zhou@mujin.co.jp>
2020-03-17 09:05:07 +0900

    Clean up apply config script.

--- bin/mujin_controllerclientpy_applyconfig.py
+++ bin/mujin_controllerclientpy_applyconfig.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+import copy
+import cStringIO
+import io
+import json
 import os
 import six
-import copy
-import json
-import argparse
-import tempfile
 import subprocess
 import tarfile
-import io
-import cStringIO
-from mujincontrollerclient.controllerclientbase import ControllerClient
+import tempfile
 
 try:
     from mujincommon import EnsureDirectory
@@ -20,7 +19,6 @@ except ImportError:
         try:
             os.makedirs(directory, **kwargs)
         except OSError:
-            # e.errno might not be errno.EEXIST, so it is better to check the directory existence.
             if not os.path.isdir(directory):
                 raise
 
@@ -30,28 +28,15 @@ except ImportError:
     def ConfigureRootLogger(level):
         logging.basicConfig(format='%(asctime)s %(name)s [%(levelname)s] [%(filename)s:%(lineno)s %(funcName)s] %(message)s', level=level)
 
+from mujincontrollerclient.controllerclientbase import ControllerClient
+
+
 import logging
 log = logging.getLogger(__name__)
 
 
-def _PrettifyConfig(config):
-    return json.dumps(config, ensure_ascii=False, indent=2, separators=(',', ': '), sort_keys=True) + '\n'
-
-
-def _DiffConfig(filename, oldconfig, newconfig):
-    with tempfile.NamedTemporaryFile(prefix='old-', suffix='-%s' % filename.replace('/', '-'), bufsize=0) as oldfile:
-        with tempfile.NamedTemporaryFile(prefix='new-', suffix='-%s' % filename.replace('/', '-'), bufsize=0) as newfile:
-            oldfile.write(_PrettifyConfig(oldconfig))
-            newfile.write(_PrettifyConfig(newconfig))
-            try:
-                subprocess.check_call(['diff', '--color', '--unified=5', oldfile.name, newfile.name])
-                return False
-            except subprocess.CalledProcessError:
-                return True
-
-
 def _ApplyTemplate(config, template, preservedpaths=None):
-    newconfig = copy.deepcopy(template)
+    newConfig = copy.deepcopy(template)
     
     # preserve the following path in the original config
     if preservedpaths is None:
@@ -100,7 +85,7 @@ def _ApplyTemplate(config, template, preservedpaths=None):
 
         # navigate to the part of the conf for preservation
         cursor = config
-        newcursor = newconfig
+        newcursor = newConfig
         skip = False
         for part in parts:
             if part not in cursor or part not in newcursor:
@@ -123,18 +108,45 @@ def _ApplyTemplate(config, template, preservedpaths=None):
         if key in cursor:
             newcursor[key] = copy.deepcopy(cursor[key])
 
-    return newconfig
+    return newConfig
+
+
+def _PrettifyConfig(content):
+    """Convert content to prettified JSON
+    """
+    return json.dumps(content, ensure_ascii=False, indent=2, separators=(',', ': '), sort_keys=True) + '\n'
+
+
+def _DiffConfig(filename, oldContent, newContent):
+    """Diff two objects in JSON format with color
+    """
+    with tempfile.NamedTemporaryFile(prefix='old-', suffix='-%s' % filename.replace('/', '-'), bufsize=0) as oldfile:
+        with tempfile.NamedTemporaryFile(prefix='new-', suffix='-%s' % filename.replace('/', '-'), bufsize=0) as newfile:
+            oldfile.write(_PrettifyConfig(oldContent))
+            newfile.write(_PrettifyConfig(newContent))
+            try:
+                subprocess.check_call(['diff', '--color', '--unified=5', oldfile.name, newfile.name])
+                return False
+            except subprocess.CalledProcessError:
+                return True
+
 
 def _IsEnsensoConfigFilename(filename):
+    """Check if a filename is ensenso or gige config
+    """
     parts = filename.split('.')
     return len(parts) == 2 and parts[0].isdigit() and parts[1] == 'json'
 
 
 def _IsSystemConfigFilename(filename):
+    """Check if a filename is system config
+    """
     return filename in ('controllersystem.conf', 'binpickingsystem.conf', 'teachworkersystem.conf')
 
 
 def _LoadConfigTar(tar):
+    """Load a collection of configs from a tar file
+    """
     config = {}
     for member in tar.getmembers():
         if not member.isfile():
@@ -152,49 +164,59 @@ def _LoadConfigTar(tar):
 
 
 def _SaveConfigTar(tar, config):
+    """Save the collection of configs into a tar file
+    """
     for filename, content in config.items():
         content = _PrettifyConfig(content)
         info = tarfile.TarInfo('config/%s' % filename)
         info.size = len(content)
-        tar.addfile(info, cStringIO.StringIO(content))
+        tar.addfile(info, io.BytesIO(content))
 
 
 def _LoadConfigDirectory(directory):
+    """Load a collection of configs from a directory
+    """
     config = {}
     for filename in os.listdir(directory):
-        fullfilename = os.path.join(directory, filename)
-        if not os.path.isfile(fullfilename):
+        fullFilename = os.path.join(directory, filename)
+        if not os.path.isfile(fullFilename):
             continue
         if not _IsSystemConfigFilename(filename):
             continue
-        with open(fullfilename, 'r') as f:
+        with open(fullFilename, 'r') as f:
             config[filename] = json.load(f)
 
     if not os.path.isdir(os.path.join(directory, 'ensenso')):
         return config
 
     for filename in os.listdir(os.path.join(directory, 'ensenso')):
-        fullfilename = os.path.join(directory, 'ensenso', filename)
-        if not os.path.isfile(fullfilename):
+        fullFilename = os.path.join(directory, 'ensenso', filename)
+        if not os.path.isfile(fullFilename):
             continue
         if not _IsEnsensoConfigFilename(filename):
             continue
-        with open(fullfilename, 'r') as f:
+        with open(fullFilename, 'r') as f:
             config['ensenso/%s' % filename] = json.load(f)
 
     return config
 
 
 def _SaveConfigDirectory(directory, config):
+    """Save a collection of configs into a directory
+    """
     for filename, content in config.items():
         content = _PrettifyConfig(content)
-        fullfilename = os.path.join(directory, filename)
-        EnsureDirectory(os.path.dirname(fullfilename))
-        with open(fullfilename, 'w') as f:
+        fullFilename = os.path.join(directory, filename)
+        EnsureDirectory(os.path.dirname(fullFilename))
+        with open(fullFilename, 'w') as f:
             f.write(content)
 
 
 def _RunMain():
+    """Main entrypoint
+    """
+
+    # first parse the options
     parser = argparse.ArgumentParser(description='Apply configuration on controller from template')
     parser.add_argument('--loglevel', action='store', type=str, dest='loglevel', default=None, help='the python log level, e.g. DEBUG, VERBOSE, ERROR, INFO, WARNING, CRITICAL [default=%(default)s]')
     parser.add_argument('--schema', action='store', type=str, dest='schema', required=True, help='path to directory containing schemas [default=%(default)s]')
@@ -204,13 +226,12 @@ def _RunMain():
     parser.add_argument('--username', action='store', type=str, dest='username', default='mujin', help='controller username [default=%(default)s]')
     parser.add_argument('--password', action='store', type=str, dest='password', default='mujin', help='controller password [default=%(default)s]')
     parser.add_argument('--force', action='store_true', dest='force', help='apply without confirmation [default=%(default)s]')
-    
     options = parser.parse_args()
 
     # configure logging
     ConfigureRootLogger(level=options.loglevel)
 
-    # check argument
+    # check argument for sanity
     if options.controller and options.config:
         log.error('need to supply only one of --controller or --config to continue')
         return
@@ -218,7 +239,7 @@ def _RunMain():
         log.error('need to supply either --controller or --config to continue')
         return
 
-    # load template
+    # load templates from directory
     template = _LoadConfigDirectory(options.template)
 
     # TODO: load schema
@@ -235,28 +256,27 @@ def _RunMain():
         # download config backup from controller
         response = client.Backup(media=False, config=True)
         with tarfile.open(fileobj=io.BytesIO(response.content), mode='r:*') as tar:
-            config = _LoadConfigDirectory(tar)
+            config = _LoadConfigTar(tar)
         
     elif options.config:
         target = options.config
         config = _LoadConfigDirectory(options.config)
 
-    # TODO: apply template
-    # newconfig = _ApplyTemplate(config, template)
-    newconfig = copy.deepcopy(config)
+    # make a copy of config
+    newConfig = copy.deepcopy(config)
 
     # debug
     from IPython.terminal import embed
     ipshell = embed.InteractiveShellEmbed(config=embed.load_default_config())(local_ns=locals())
 
     # find what is changed
-    changedconfig = {}
-    for filename, newcontent in newconfig.items():
-        if _DiffConfig(filename, config.get(filename), newcontent):
-            changedconfig[filename] = newcontent
+    changedConfig = {}
+    for filename, newContent in newConfig.items():
+        if _DiffConfig(filename, config.get(filename), newContent):
+            changedConfig[filename] = newContent
 
     # if the config is different, prompt the user
-    if len(changedconfig) == 0:
+    if len(changedConfig) == 0:
         log.debug('configuration already up-to-date on %s', target)
         return
 
@@ -273,14 +293,15 @@ def _RunMain():
     log.debug('applying configuration on %s', target)
     if options.controller:
         # restore configurations
-        output = cStringIO.StringIO()
+        output = io.BytesIO()
         with tarfile.open(fileobj=output, mode='w|gz') as tar:
-            _SaveConfigTar(tar, changedconfig)
+            _SaveConfigTar(tar, changedConfig)
         output.seek(0)
         client.RestoreBackup(output, media=False, config=True)
     elif options.config:
         # write out the entire directory
-        _SaveConfigDirectory(options.config, changedconfig)
+        _SaveConfigDirectory(options.config, changedConfig)
+
     log.debug('done')
 
 
