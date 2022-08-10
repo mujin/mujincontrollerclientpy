838163be6b687931f84771ae413de11f4d747602
Taiju Yamada <taiju.yamada@mujin.co.jp>
2022-08-10 14:31:25 +0900

    use setsockopt_string

--- python/mujincontrollerclient/planningclient.py
+++ python/mujincontrollerclient/planningclient.py
@@ -154,7 +154,7 @@ class PlanningControllerClient(controllerclientbase.ControllerClient):
             socket.setsockopt(zmq.TCP_KEEPALIVE_INTVL, 2) # the interval between subsequential keepalive probes, regardless of what the connection has exchanged in the meantime
             socket.setsockopt(zmq.TCP_KEEPALIVE_CNT, 2) # the number of unacknowledged probes to send before considering the connection dead and notifying the application layer
             socket.connect('tcp://%s:%s' % (self.controllerIp, self.taskheartbeatport))
-            socket.setsockopt(zmq.SUBSCRIBE, '')
+            socket.setsockopt_string(zmq.SUBSCRIBE, '')
             poller = zmq.Poller()
             poller.register(socket, zmq.POLLIN)
             
