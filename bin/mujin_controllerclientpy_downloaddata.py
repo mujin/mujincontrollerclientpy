c5a4084c923fb68d1ed0cbd9cc85003a838ae2bf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 17:29:59 +0000

    Adding tarfile member sanitization to extractall()

--- bin/mujin_controllerclientpy_downloaddata.py
+++ bin/mujin_controllerclientpy_downloaddata.py
@@ -73,7 +73,26 @@ def _DownloadBackup(controllerClient, sceneList, timeout=600.0):
     downloadDirectory = os.path.join(os.getcwd(), archiveFilename.rstrip('.tar.gz'))
     log.info('downloading and saving data to: %s', downloadDirectory)
     with tarfile.open(fileobj=response.raw, mode='r|gz') as tar:
-        tar.extractall(path=downloadDirectory)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=downloadDirectory)
     log.info('download completed, data saved to: %s', downloadDirectory)
 
 def _Main():
