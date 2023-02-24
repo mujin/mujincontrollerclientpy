5074e125753ed3b53913f541949d21ae0b39a6ff
Sergey Pavlov <cap.obvios@gmail.com>
2023-02-24 15:39:41 +0900

    [Manual Recovery] Propagate dynamicGoalsGeneratorParameters (#4)
    * [Manual Recovery] Propagate dynamicGoalsGeneratorParameters  to ManuallyPlacePackItem() to compute placement position same to robot execution.
    
    ---------
    
    Co-authored-by: sergey.pavlov <sergey.pavlov@mujin.co.jp>

--- python/mujinplanningclient/binpickingplanningclient.py
+++ python/mujinplanningclient/binpickingplanningclient.py
@@ -576,7 +576,7 @@ class BinpickingPlanningClient(realtimerobotplanningclient.RealtimeRobotPlanning
         taskparameters.update(kwargs)
         return self.ExecuteCommand(taskparameters, timeout=timeout, fireandforget=fireandforget)
 
-    def ManuallyPlacePackItem(self, packFormationComputationResult=None, inputPartIndex=None, placeLocationNames=None, placedTargetPrefix=None, orderNumber=None, numLeftToPick=None, timeout=10, fireandforget=False):
+    def ManuallyPlacePackItem(self, packFormationComputationResult=None, inputPartIndex=None, placeLocationNames=None, placedTargetPrefix=None, dynamicGoalsGeneratorParameters=None, orderNumber=None, numLeftToPick=None, timeout=10, fireandforget=False):
         """
         Places an item according to the pack formation assuming the item is placed manually and updates robotbridge state
         """
@@ -594,6 +594,8 @@ class BinpickingPlanningClient(realtimerobotplanningclient.RealtimeRobotPlanning
             taskparameters['numLeftToPick'] = numLeftToPick
         if placedTargetPrefix:
             taskparameters['placedTargetPrefix'] = placedTargetPrefix
+        if dynamicGoalsGeneratorParameters is not None:
+            taskparameters['dynamicGoalsGeneratorParameters'] = dynamicGoalsGeneratorParameters
         return self.ExecuteCommand(taskparameters, timeout=timeout, fireandforget=fireandforget)
 
     def SendPackFormationComputationResult(self, timeout=10, fireandforget=False, **kwargs):
